5200415bf3b26fb761e604399eb0f611d3651c88
‘Matthew <matthew.lindly@hotmail.com>
2016-10-19 16:42:32 -0600

    Progress.

--- js/init.js
+++ js/init.js
@@ -2,4 +2,15 @@
 
 $(document).ready(function(){
      $('.parallax').parallax();
+     $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 240
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      }
+);
 });
+
+// Initialize collapse button
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
