6b9e0f2aefe4cf992403b7065504b63f6393a1be
‘Matthew <matthew.lindly@hotmail.com>
2016-10-20 14:46:45 -0600

    Added ugly register & sign-up pop-up window.

--- js/init.js
+++ js/init.js
@@ -2,15 +2,5 @@
 
 $(document).ready(function(){
      $('.parallax').parallax();
-     $('.button-collapse').sideNav({
-        menuWidth: 300, // Default is 240
-        edge: 'right', // Choose the horizontal origin
-        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      }
-);
+     $('.modal-trigger').leanModal();
 });
-
-// Initialize collapse button
-$(".button-collapse").sideNav();
-// Initialize collapsible (uncomment the line below if you use the dropdown variation)
-//$('.collapsible').collapsible();
