5bec418a7c5ff282cda823fd6c90f9414d59d3aa
‘Matthew <matthew.lindly@hotmail.com>
2016-10-27 13:52:52 -0600

    Daily commit.

--- games/blackjack/public/javascripts/filters.js
+++
@@ -1,8 +0,0 @@
-app.filter('capitalize', function() {
-    return function(input) {
-        if (input) {
-          return input[0].toUpperCase() + input.slice(1);
-        }
-        return null;
-    }
-});
