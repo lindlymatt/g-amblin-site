e8b52b22b563e2266fa30a28288e50105937e058
‘Matthew <matthew.lindly@hotmail.com>
2016-11-03 12:09:22 -0600

    Finished most war functionality.

--- games/war/war.js
+++ games/war/war.js
@@ -18,55 +18,45 @@ var thePot = parseInt(thePlayerBet) * 2;
 var theDeck = null;
 var playerScore = 0;
 var dealerScore = 0;
-var hiddenCard = '';
-var hiddenValue = 0;
-var playerHand = [];
-var dealerHand = [];
-var dClicked = false;
-var pClicked = false;
+var realPValue = 0;
+var realDValue = 0;
+var dHiddenCard = '';
+var pHiddenCard = '';
+var dHiddenValue = 0;
+var pHiddenValue = 0;
+// var playerHand = [];
+// var dealerHand = [];
+// var dClicked = false;
+// var pClicked = false;
 var playerCards = $('.player-cards');
 var dealerCards = $('.dealer-cards');
 var playerScoreDis = $('#player-score');
 var dealerScoreDis = $('#dealer-score');
-var hitButton = $('#hit-button');
+var warButton = $('#war-button');
 var stayButton = $('#stay-button');
 
 $(function() {
 
   // Shows Accurate Bet
   thePotText.text('$' + thePot);
+  stayButton.hide();
 
   // Generate Initial Hands
   generateDeck().then(data => {
     theDeck = data;
     drawCards(theDeck, 1).then(data => displayCard(data, dealerCards));
     drawCards(theDeck, 1).then(data => displayCard(data, playerCards));
-    drawCards(theDeck, 1).then(data => displayCard(data, dealerCards));
-    drawCards(theDeck, 1).then(data => displayCard(data, playerCards));
   });
 
-  // Hit Button Functionality
-  hitButton.on('click', hitPlayerCard);
-
   // Stay Button Functionality
-  stayButton.on('click', function() {
-    dClicked = true;
-    hitButton.hide();
-    dealerCards.children().last().attr('src', hiddenCard);
-    // dealerScore += parseInt(hiddenValue);
-    updateValues(dealerScore);
-    hitDealerCard();
+  warButton.on('click', function() {
+    // dClicked = true;
+    dealerCards.children().id(0).attr('src', dHiddenCard);
+    playerCards.children().id(0).attr('src', pHiddenCard);
+    // updateValues(dealerScore);
     dealerScoreDis.text(dealerScore);
+    playerScoreDis.text(playerScore);
 
-    // if(dealerScore === playerScore) {
-    //   pushedHands();
-    // }
-    // else if(playerScore > dealerScore) {
-    //   alert('Player wins!');
-    // }
-    // else {
-    //   alert('Dealer wins!');
-    // }
     setTimeout(testWinner, 1000);
   });
 });
@@ -79,34 +69,50 @@ function drawCards(deck, num) {
   return $.getJSON('https://deckofcardsapi.com/api/deck/' + deck.deck_id + '/draw/?count=' + num);
 }
 
-function hitPlayerCard() {
-  pClicked = true;
-  return drawCards(theDeck, 1).then(data => displayCard(data, playerCards));
+function gameOfWar() {
+  var $bustScreen = $('#expanded');
+  var $bustText = $('.first-line');
+  var $bustText2 = $('.second-line');
+  var warButton = $('#war-war-button');
+  var warStayButton = $('#war-stay-button');
+
+  $bustScreen.fadeIn(200).delay(3000).fadeOut(200);
+  $bustText.text('SAME HAND, SHALL WE GENTS?!');
+  $bustText2.text('DO NOT BE A SALLY! LET US GO TO WAR!');
+
+  warButton.on('click', function() {
+
+  });
 }
 
-function hitDealerCard() {
-  if (dealerScore < 17) {
-    drawCards(theDeck, 1).then(data => {
-      displayCard(data, dealerCards);
-      hitDealerCard();
-    });
-  }
-}
+// function hitPlayerCard() {
+//   return drawCards(theDeck, 1).then(data => displayCard(data, playerCards));
+// }
+
+// function hitDealerCard() {
+//   if (dealerScore < 17) {
+//     drawCards(theDeck, 1).then(data => {
+//       displayCard(data, dealerCards);
+//       hitDealerCard();
+//     });
+//   }
+// }
 
 function displayCard(data, side) {
     var $img = $('<img>');
     var cardVal = data.cards[0].value;
-    if(dealerCards.children().length === 1 && side === dealerCards) {
-      if(cardVal === "JACK" || cardVal === "QUEEN" || cardVal === "KING") {
-        hiddenValue = 10;
-      }
-      else if(cardVal === "ACE") {
-        hiddenValue = 11;
-      }
-      else {
-        hiddenValue = parseInt(cardVal);
-      }
-      hiddenCard = data.cards[0].image;
+
+    if(dealerCards.children().length === 0 && side === dealerCards) {
+      dHiddenValue = cardVal;
+      dHiddenCard = data.cards[0].image;
+      $img.attr('src', 'images/back-of-card.jpg');
+      $img.attr('height', '150px');
+      side.append($img);
+      grabValues(data.cards[0], side);
+    }
+    else if(playerCards.children().length === 0 && side === playerCards) {
+      pHiddenValue = cardVal;
+      pHiddenCard = data.cards[0].image;
       $img.attr('src', 'images/back-of-card.jpg');
       $img.attr('height', '150px');
       side.append($img);
@@ -122,154 +128,38 @@ function displayCard(data, side) {
 
 function grabValues(card, side) {
   if(side === playerCards) {
-    if(card.value === "KING" || card.value === "QUEEN" || card.value === "JACK") {
-      playerHand.push(10);
-      playerScore += 10;
-      updateValues(playerScore);
+    if(card.value === "ACE") {
+      realPValue = 14;
     }
-    else if(card.value === "ACE") {
-      playerHand.push('ACE');
-      playerScore += 11;
-      updateValues(playerScore);
+    else if(card.value === "KING") {
+      realPValue = 13;
+    }
+    else if(card.value === "QUEEN") {
+      realPValue = 12;
+    }
+    else if(card.value === "JACK") {
+      realPValue = 11;
     }
     else {
-      playerHand.push(card.value);
-      playerScore += parseInt(card.value);
-      updateValues(playerScore);
+      realPValue = card.value;
     }
   }
   else {
-    if(card.value === "KING" || card.value === "QUEEN" || card.value === "JACK") {
-      dealerHand.push(10);
-      dealerScore += 10;
-      updateValues(dealerScore);
+    if(card.value === "ACE") {
+      realDValue = 14;
     }
-    else if(card.value === "ACE") {
-      dealerHand.push('ACE');
-      dealerScore += 11;
-      updateValues(dealerScore);
+    else if(card.value === "KING") {
+      realDValue = 13;
     }
-    else {
-      dealerHand.push(card.value);
-      dealerScore += parseInt(card.value);
-      updateValues(dealerScore);
+    else if(card.value === "QUEEN") {
+      realDValue = 12;
     }
-  }
-}
-
-function testAce(hand) {
-  if(hand === playerHand) {
-    if(hand.includes('ACE')) {
-      var pIndexOf = hand.indexOf('ACE');
-      if((playerScore - 11) < 21) {
-        playerScore = (playerScore - 11) + 1;
-        playerHand[pIndexOf] = 1;
-        updateValues(playerScore);
-      }
-      else {
-        playerScoreDis.text(playerScore);
-        bustedHand(playerCards);
-      }
+    else if(card.value === "JACK") {
+      realDValue = 11;
     }
     else {
-      playerScoreDis.text(playerScore);
-      bustedHand(playerCards);
+      realDValue = card.value;
     }
-  }
-  else {
-    if(hand.includes('ACE')) {
-      var dIndexOf = hand.indexOf('ACE');
-      if((dealerScore - 11) < 22) {
-        dealerScore = (dealerScore - 11) + 1;
-        dealerHand[dIndexOf] = 1;
-        updateValues(dealerScore);
-      }
-      else {
-        dealerScoreDis.text(dealerScore);
-        bustedHand(dealerCards);
-      }
-    }
-    else {
-      dealerScoreDis.text(dealerScore);
-      bustedHand(dealerCards);
-    }
-  }
-}
-
-function updateValues(score) {
-  if(score === playerScore) {
-    if(playerScore > 21 && playerCards.children().length > 1) {
-      testAce(playerHand);
-    }
-    else if(playerScore === 21 && pClicked === false) {
-      hitButton.hide();
-      playerScoreDis.text(playerScore);
-      dealerCards.children().last().attr('src', hiddenCard);
-      dealerScoreDis.text(dealerScore);
-      var $bustScreen = $('#expanded');
-      var $bustText = $('.first-line');
-      $bustScreen.fadeIn(200).delay(3000).fadeOut(200);
-      $bustText.text('BLACKJACK! YOU WIN ' + (thePot / 2) + ' TOKENS!');
-      localStorage.setItem('tokens', (parseInt(localStorage.getItem('tokens')) + parseInt(thePot)));
-      resetGame();
-    }
-    else {
-      playerScoreDis.text(score);
-    }
-  }
-  else {
-    if(dealerScore > 21) {
-      testAce(dealerHand);
-    }
-    else if(dClicked === false && dealerScore !== 22) {
-      console.log(dealerScore);
-      dealerScoreDis.text(score - hiddenValue);
-    }
-    else if(dClicked === true && dealerScore === playerScore) {
-      dealerScoreDis.text(score);
-      pushedHands();
-    }
-    else {
-      dealerScoreDis.text(score);
-    }
-  }
-}
-
-// Load the "Bust" Screen
-function bustedHand(side) {
-  // dealerScore = 0;
-  var $bustScreen = $('#expanded');
-  var $bustText = $('.first-line');
-  var $bustText2 = $('.second-line');
-  $bustScreen.fadeIn(200).delay(3000).fadeOut(200);
-
-  if(side === playerCards) {
-    dealerCards.children().last().attr('src', hiddenCard);
-    dealerScoreDis.text(dealerScore);
-    $bustText.text('YOU BUSTED!');
-    $bustText2.text('YOU LOST YOUR ' + (thePot / 2) + ' TOKENS!');
-  }
-  else {
-    $bustText.text('DEALER BUST!');
-    $bustText2.text('YOU WIN ' + (thePot / 2) + ' TOKENS!');
-    localStorage.setItem('tokens', (parseInt(localStorage.getItem('tokens')) + parseInt(thePot)));
-  }
-
-  resetGame();
-}
-
-// Load the "Push" Screen
-function pushedHands() {
-  if(dealerScore > 16) {
-    dealerScoreDis.text(dealerScore);
-    var $bustScreen = $('#expanded');
-    var $bustText = $('.first-line');
-    var $bustText2 = $('.second-line');
-    $bustScreen.fadeIn(200).delay(3000).fadeOut(200);
-    $bustText.text('PUSH, SAME HANDS!');
-    $bustText2.text('YOU KEEP YOUR ' + (parseInt(thePot) / 2) + ' TOKENS!');
-    localStorage.setItem('tokens', (parseInt(localStorage.getItem('tokens')) + parseInt(thePot / 2)));
-    resetGame();
   }
 }
 
@@ -277,46 +167,61 @@ function testWinner() {
   var $bustScreen = $('#expanded');
   var $bustText = $('.first-line');
   var $bustText2 = $('.second-line');
+  var warButton = $('#war-war-button').hide();
+  var warStayButton = $('#war-stay-button').hide();
   // var $scoresText = $('<div>').append($('<h1>'));
   // $scoresText.first().addId('expanded-text');
 
-  if(playerScore === dealerScore && dealerScore < 22 && playerScore < 22) {
-    pushedHands();
-  }
-  if(playerScore < 22 && playerScore > dealerScore) {
+  if(parseInt(realPValue) > parseInt(realDValue)) {
     $bustScreen.fadeIn(200).delay(3000).fadeOut(200);
-    $bustText.text('YOU HAVE A HIGHER HAND!');
+    $bustText.text('YOU HAVE A HIGHER CARD!');
     $bustText2.text('YOU WIN ' + (thePot / 2) + ' TOKENS!');
     localStorage.setItem('tokens', (parseInt(localStorage.getItem('tokens')) + parseInt(thePot)));
     // $scoresText.first().text('DEALER SCORE: ' + dealerScore + ' vs. PLAYER SCORE: ' + playerScore);
     resetGame();
   }
-  if(dealerScore < 22 && dealerScore > 16 && dealerScore > playerScore) {
+  else if(parseInt(realPValue) < parseInt(realDValue)) {
+      $bustScreen.fadeIn(200).delay(3000).fadeOut(200);
+      $bustText.text('DEALER HAS HIGHER CARD!');
+      $bustText2.text('YOU LOST YOUR ' + (thePot / 2) + ' TOKENS!');
+      // $scoresText.first().text('DEALER SCORE: ' + dealerScore + ' vs. PLAYER SCORE: ' + playerScore);
+      resetGame();
+  }
+  else {
     $bustScreen.fadeIn(200).delay(3000).fadeOut(200);
-    $bustText.text('DEALER HAS HIGHER HAND!');
-    $bustText2.text('YOU LOST YOUR ' + (thePot / 2) + ' TOKENS!');
-    // $scoresText.first().text('DEALER SCORE: ' + dealerScore + ' vs. PLAYER SCORE: ' + playerScore);
-    resetGame();
+    warButton.show();
+    warStayButton.show();
+    $bustText.text('SAME HAND, SHALL WE GENTS?!');
+    $bustText2.text('DO NOT BE A SALLY! LET US GO TO WAR!');
+
+    warButton.on('click', function() {
+      warButton.hide();
+      gameOfWar();
+    });
+
+    warStayButton.on('click', function() {
+      localStorage.setItem('tokens', (parseInt(localStorage.getItem('tokens')) + parseInt(thePot / 2)));
+      resetGame();
+    });
   }
 }
 
 function resetGame() {
   var $potText = $('.pot-text');
   var $potHeader = $('.pot-header');
-  hitButton.off('click');
-  stayButton.off('click');
-  hitButton.text('SAME BET').show();
-  stayButton.text('NEW BET');
+  warButton.off('click');
+  warButton.text('SAME BET');
+  stayButton.text('NEW BET').show();
   $potText.text('');
   $potHeader.text('PLAY AGAIN?');
   console.log(localStorage.getItem('tokens'));
 
-  hitButton.on('click', function() {
+  warButton.on('click', function() {
     if(parseInt(thePlayerBet) <= parseInt(localStorage.getItem('tokens'))) {
       localStorage.setItem('tokens', (parseInt(localStorage.getItem('tokens')) - parseInt(thePlayerBet)));
       console.log(playerTokens);
       $mainContent.load('partials/game.partial', function() {
-        $.getScript('blackjack.js');
+        $.getScript('war.js');
       });
     }
     else {
@@ -341,7 +246,7 @@ function resetGame() {
           else {
             localStorage.setItem('tokens', (parseInt(playerTokens) - parseInt(thePlayerBet)));
             $mainContent.load("partials/game.partial", function() {
-              $.getScript('blackjack.js');
+              $.getScript('war.js');
             });
           }
         });
